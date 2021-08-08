09ae04b4a295d3c1f8230642ddb5abfef90aadcd
Jael Armas M <jaelarmas.m@gmail.com>
2021-08-07 22:40:23 -0500

    Validate inputs and renderQuote function

--- src/App.js
+++ src/App.js
@@ -4,71 +4,106 @@ import planeIcon from './assets/airplane.png';
 import shipIcon from './assets/ship.png';
 
 function App() {
+
+  const [quote, setQuote] = useState(null);
+  const [initInfo, setInitInfo] = useState({
+    startingCountry: '',
+    destinationCountry: '',
+    quotePrice: '',
+    shippingChannel: 'air'
+  });
+
+  const handleStartingCountryInput = (e) => {
+    if (e.target.value.match(/^[a-zA-ZÀ-ÿ ]+$|^$/i))
+      setInitInfo({ ...initInfo, startingCountry: e.target.value });
+  }
+
+  const handleDestinationCountryInput = (e) => {
+    if (e.target.value.match(/^[a-zA-ZÀ-ÿ ]+$|^$/i))
+      setInitInfo({ ...initInfo, destinationCountry: e.target.value });
+  }
+
+  const handleQuotePriceInput = (e) => {
+    if (e.target.value.match(/^[0-9]+$|^$/))
+      setInitInfo({ ...initInfo, quotePrice: e.target.value });
+  }
+
+  const handleShippingChannelSelect = (e) => {
+    setInitInfo({ ...initInfo, shippingChannel: e.target.value });
+  }
+
+  const renderQuote = () => {
+    const icon = initInfo.shippingChannel === 'ocean' ? shipIcon : planeIcon;
+    const text = initInfo.shippingChannel === 'ocean' ? 'Traditional ocean freight' : 'Traditional air freight';
+    setQuote(
+      <div className="App-quote-box">
+        <table>
+          <thead>
+            <tr>
+              <th>
+                <div className="Table-channel">
+                  <img src={icon} width={50} height={50} alt={initInfo.shippingChannel} />
+                  <span>{text}</span>
+                </div>
+              </th>
+              <th className="Table-countries-cell">
+                <div className="Table-countries">
+                  <span>{initInfo.startingCountry + ' -> ' + initInfo.destinationCountry}</span>
+                </div>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <p className="Table-number-days">4 - 6 days</p>
+                <p className="Table-estimated">Estimated delivery</p>
+                <p className="Table-dates">Sept 20 - Sept 26</p>
+              </td>
+              <td>
+                <div className="Table-price">
+                  <span>US$ 2300</span>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+
   return (
     <div className="App">
+      {console.log(initInfo)}
       <header className="App-header">
-        <span className="App-title">
-          Generate Quote
-        </span>
+        <span className="App-title">Generate Quote</span>
       </header>
       <div className="App-container">
-        <form className="App-form">
+        <div className="App-form">
           <div className="App-input-group">
             <label>Starting country</label>
-            <input type="text" placeholder="Country name" />
+            <input type="text" placeholder="Country name" pattern="[a-zA-ZÀ-ÿ ]+" value={initInfo.startingCountry} onChange={handleStartingCountryInput} />
           </div>
           <div className="App-input-group">
             <label>Destination country</label>
-            <input type="text" placeholder="Country name" />
+            <input type="text" placeholder="Country name" pattern="[a-zA-ZÀ-ÿ ]+" value={initInfo.destinationCountry} onChange={handleDestinationCountryInput} />
           </div>
           <div className="App-input-group">
             <label>Quote price</label>
-            <input type="number" placeholder="Price" />
+            <input type="number" placeholder="Price" pattern="^0\d{9}$" value={initInfo.quotePrice} onChange={handleQuotePriceInput} />
           </div>
           <div className="App-input-group">
             <label>Shipping channel</label>
-            <select>
+            <select onChange={handleShippingChannelSelect}>
               <option value="air">Air</option>
               <option value="ocean">Ocean</option>
             </select>
           </div>
           <div className="App-input-group">
-            <button className="App-button">Create quote</button>
+            <button className="App-button" onClick={renderQuote} disabled={initInfo.startingCountry.match(/^$/) || initInfo.destinationCountry.match(/^$/) || initInfo.quotePrice.match(/^$/)} >Create quote</button>
           </div>
-        </form>
-        <div className="App-quote-box">
-          <table>
-            <thead>
-              <tr>
-                <th>
-                  <div className="Table-channel">
-                    <img src={planeIcon} width={50} height={50} />
-                    <span>{'Traditional air freight'}</span>
-                  </div>
-                </th>
-                <th className="Table-countries-cell">
-                  <div className="Table-countries">
-                    <span>{'China -> USA'}</span>
-                  </div>
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>
-                  <p className="Table-number-days">4 - 6 days</p>
-                  <p className="Table-estimated">Estimated delivery</p>
-                  <p className="Table-dates">Sept 20 - Sept 26</p>
-                </td>
-                <td>
-                  <div className="Table-price">
-                    <span>US$ 2300</span>
-                  </div>
-                </td>
-              </tr>
-            </tbody>
-          </table>
         </div>
+        {quote}
       </div>
     </div>
   );
